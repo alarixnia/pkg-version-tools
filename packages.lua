9c31afab64546d48efaf0ed5206230000639eb1d
nia <nia@NetBSD.org>
2019-09-17 21:58:25 +0100

    packages: indent. looks better...

--- packages.lua
+++ packages.lua
@@ -1,336 +1,336 @@
 return {
-["audio/audacious"] =
-{
-wikidata = "Q758685"
-},
-["audio/audacity"] =
-{
-wikidata = "Q191782",
-freshcode = "audacity"
-},
-["audio/cmus"] =
-{
-wikidata = "Q1940750"
-},
-["audio/exaile"] =
-{
-wikidata = "Q947786",
-freshcode = "exaile"
-},
-["audio/flac"] =
-{
-wikidata = "Q219848",
-freshcode = "flac"
-},
-["audio/lame"] =
-{
-wikidata = "Q902513"
-},
-["audio/libopus"] =
-{
-wikidata = "Q28975307"
-},
-["audio/libsndfile"] =
-{
-wikidata = "Q1344172"
-},
-["audio/libvorbis"] =
-{
-wikidata = "Q11885120",
-freshcode = "libvorbis"
-},
-["audio/libwildmidi"] =
-{
-wikidata = "Q16967611"
-},
-["audio/lmms"] =
-{
-wikidata = "Q201809"
-},
-["audio/mpg123"] =
-{
-wikidata = "Q287021"
-},
-["audio/musicpd"] =
-{
-wikidata = "Q1257936"
-},
-["audio/opusfile"] =
-{
-wikidata = "Q64585145"
-},
-["audio/speex"] =
-{
-wikidata = "Q29024883"
-},
-["audio/taglib"] =
-{
-wikidata = "Q2387630"
-},
-["chat/anope"] =
-{
-wikidata = "Q2852313"
-},
-["chat/atheme"] =
-{
-wikidata = "Q28941620"
-},
-["chat/bitlbee"] =
-{
-wikidata = "Q878823",
-freshcode = "bitlbee"
-},
-["chat/hexchat"] =
-{
-wikidata = "Q17117379"
-},
-["chat/inspircd3"] =
-{
-wikidata = "Q1531183"
-},
-["chat/irssi"] =
-{
-wikidata = "Q746544"
-},
-["chat/mumble"] =
-{
-wikidata = "Q748932"
-},
-["chat/pidgin"] =
-{
-wikidata = "Q71380",
-freshcode = "pidgin"
-},
-["chat/quassel"] =
-{
-wikidata = "Q921961"
-},
-["chat/unrealircd"] =
-{
-wikidata = "Q944364"
-},
-["chat/weechat"] =
-{
-wikidata = "Q2000469"
-},
-["converters/fribidi"] =
-{
-wikidata = "Q11201268"
-},
-["devel/SDL2"] =
-{
-wikidata = "Q727439"
-},
-["devel/pango"] =
-{
-wikidata = "Q591485"
-},
-["editors/abiword"] =
-{
-wikidata = "Q136775",
-freshcode = "abiword"
-},
-["emulators/dolphin-emu"] =
-{
-wikidata = "Q1236429"
-},
-["emulators/dosbox"] =
-{
-wikidata = "Q479783"
-},
-["emulators/mame"] =
-{
-wikidata = "Q1054055"
-},
-["emulators/mednafen"] =
-{
-wikidata = "Q596918"
-},
-["emulators/qemu"] =
-{
-wikidata = "Q624699",
-freshcode = "qemu"
-},
-["emulators/snes9x"] =
-{
-wikidata = "Q3309521"
-},
-["fonts/fontconfig"] =
-{
-wikidata = "Q1017060"
-},
-["fonts/harfbuzz"] =
-{
-wikidata = "Q4119352"
-},
-["games/7kaa"] =
-{
-wikidata = "Q66105134"
-},
-["games/minetest"] =
-{
-wikidata = "Q722334"
-},
-["games/openmw"] =
-{
-wikidata = "Q21758353"
-},
-["games/openttd"] =
-{
-wikidata = "Q725120"
-},
-["games/powder-toy"] =
-{
-wikidata = "Q10853009"
-},
-["graphics/GraphicsMagick"] =
-{
-wikidata = "Q4039670"
-},
-["graphics/MesaLib"] =
-{
-wikidata = "Q1778544",
-freshcode = "mesa"
-},
-["graphics/blender"] =
-{
-wikidata = "Q173136",
-freshcode = "blender"
-},
-["graphics/cairo"] =
-{
-wikidata = "Q306026"
-},
-["graphics/freetype2"] =
-{
-wikidata = "Q1326617",
-freshcode = "freetype"
-},
-["graphics/gdk-pixbuf2"] =
-{
-wikidata = "Q47272781"
-},
-["graphics/gimp"] =
-{
-wikidata = "Q8038",
-freshcode = "gimp"
-},
-["graphics/geeqie"] =
-{
-wikidata = "Q9394384"
-},
-["graphics/jpeg"] =
-{
-wikidata = "Q131741"
-},
-["graphics/lcms2"] =
-{
-wikidata = "Q1502002"
-},
-["graphics/libexif"] =
-{
-wikidata = "Q28515920"
-},
-["graphics/libraw"] =
-{
-wikidata = "Q28517022"
-},
-["graphics/librsvg"] =
-{
-wikidata = "Q286024"
-},
-["graphics/libwebp"] =
-{
-wikidata = "Q63437758"
-},
-["graphics/openjpeg"] =
-{
-wikidata = "Q3353118"
-},
-["graphics/png"] =
-{
-wikidata = "Q838041"
-},
-["graphics/tiff"] =
-{
-wikidata = "Q1017110"
-},
-["mail/balsa"] =
-{
-wikidata = "Q401995"
-},
-["mail/mutt"] =
-{
-wikidata = "Q1457398"
-},
-["multimedia/libogg"] =
-{
-wikidata = "Q63437792"
-},
-["multimedia/vlc"] =
-{
-wikidata = "Q171477",
-freshcode = "vlc"
-},
-["net/filezilla"] =
-{
-wikidata = "Q300662",
-freshcode = "filezilla"
-},
-["net/haproxy"] =
-{
-wikidata = "Q5628948",
-freshcode = "haproxy"
-},
-["net/remmina"] =
-{
-wikidata = "Q82148"
-},
-["net/rtorrent"] =
-{
-wikidata = "Q284842"
-},
-["net/syncthing"] =
-{
-wikidata = "Q17089344"
-},
-["net/transmission"] =
-{
-wikidata = "Q875055"
-},
-["security/gnutls"] =
-{
-wikidata = "Q1533305"
-},
-["security/libgcrypt"] =
-{
-wikidata = "Q3237868"
-},
-["security/putty"] =
-{
-wikidata = "Q841685",
-freshcode = "putty"
-},
-["textproc/libxslt"] =
-{
-wikidata = "Q6542808"
-},
-["www/gitea"] =
-{
-wikidata = "Q28714270"
-},
-["www/lighttpd"] =
-{
-wikidata = "Q217765",
-freshcode = "lighttpd"
-},
-["www/seamonkey"] =
-{
-wikidata = "Q156197",
-freshcode = "seamonkey"
-},
-["www/thttpd"] =
-{
-wikidata = "Q3266371"
-}
+	["audio/audacious"] =
+	{
+		wikidata = "Q758685"
+	},
+	["audio/audacity"] =
+	{
+		wikidata = "Q191782",
+		freshcode = "audacity"
+	},
+	["audio/cmus"] =
+	{
+		wikidata = "Q1940750"
+	},
+	["audio/exaile"] =
+	{
+		wikidata = "Q947786",
+		freshcode = "exaile"
+	},
+	["audio/flac"] =
+	{
+		wikidata = "Q219848",
+		freshcode = "flac"
+	},
+	["audio/lame"] =
+	{
+		wikidata = "Q902513"
+	},
+	["audio/libopus"] =
+	{
+		wikidata = "Q28975307"
+	},
+	["audio/libsndfile"] =
+	{
+		wikidata = "Q1344172"
+	},
+	["audio/libvorbis"] =
+	{
+		wikidata = "Q11885120",
+		freshcode = "libvorbis"
+	},
+	["audio/libwildmidi"] =
+	{
+		wikidata = "Q16967611"
+	},
+	["audio/lmms"] =
+	{
+		wikidata = "Q201809"
+	},
+	["audio/mpg123"] =
+	{
+		wikidata = "Q287021"
+	},
+	["audio/musicpd"] =
+	{
+		wikidata = "Q1257936"
+	},
+	["audio/opusfile"] =
+	{
+		wikidata = "Q64585145"
+	},
+	["audio/speex"] =
+	{
+		wikidata = "Q29024883"
+	},
+	["audio/taglib"] =
+	{
+		wikidata = "Q2387630"
+	},
+	["chat/anope"] =
+	{
+		wikidata = "Q2852313"
+	},
+	["chat/atheme"] =
+	{
+		wikidata = "Q28941620"
+	},
+	["chat/bitlbee"] =
+	{
+		wikidata = "Q878823",
+		freshcode = "bitlbee"
+	},
+	["chat/hexchat"] =
+	{
+		wikidata = "Q17117379"
+	},
+	["chat/inspircd3"] =
+	{
+		wikidata = "Q1531183"
+	},
+	["chat/irssi"] =
+	{
+		wikidata = "Q746544"
+	},
+	["chat/mumble"] =
+	{
+		wikidata = "Q748932"
+	},
+	["chat/pidgin"] =
+	{
+		wikidata = "Q71380",
+		freshcode = "pidgin"
+	},
+	["chat/quassel"] =
+	{
+		wikidata = "Q921961"
+	},
+	["chat/unrealircd"] =
+	{
+		wikidata = "Q944364"
+	},
+	["chat/weechat"] =
+	{
+		wikidata = "Q2000469"
+	},
+	["converters/fribidi"] =
+	{
+		wikidata = "Q11201268"
+	},
+	["devel/SDL2"] =
+	{
+		wikidata = "Q727439"
+	},
+	["devel/pango"] =
+	{
+		wikidata = "Q591485"
+	},
+	["editors/abiword"] =
+	{
+		wikidata = "Q136775",
+		freshcode = "abiword"
+	},
+	["emulators/dolphin-emu"] =
+	{
+		wikidata = "Q1236429"
+	},
+	["emulators/dosbox"] =
+	{
+		wikidata = "Q479783"
+	},
+	["emulators/mame"] =
+	{
+		wikidata = "Q1054055"
+	},
+	["emulators/mednafen"] =
+	{
+		wikidata = "Q596918"
+	},
+	["emulators/qemu"] =
+	{
+		wikidata = "Q624699",
+		freshcode = "qemu"
+	},
+	["emulators/snes9x"] =
+	{
+		wikidata = "Q3309521"
+	},
+	["fonts/fontconfig"] =
+	{
+		wikidata = "Q1017060"
+	},
+	["fonts/harfbuzz"] =
+	{
+		wikidata = "Q4119352"
+	},
+	["games/7kaa"] =
+	{
+		wikidata = "Q66105134"
+	},
+	["games/minetest"] =
+	{
+		wikidata = "Q722334"
+	},
+	["games/openmw"] =
+	{
+		wikidata = "Q21758353"
+	},
+	["games/openttd"] =
+	{
+		wikidata = "Q725120"
+	},
+	["games/powder-toy"] =
+	{
+		wikidata = "Q10853009"
+	},
+	["graphics/GraphicsMagick"] =
+	{
+		wikidata = "Q4039670"
+	},
+	["graphics/MesaLib"] =
+	{
+		wikidata = "Q1778544",
+		freshcode = "mesa"
+	},
+	["graphics/blender"] =
+	{
+		wikidata = "Q173136",
+		freshcode = "blender"
+	},
+	["graphics/cairo"] =
+	{
+		wikidata = "Q306026"
+	},
+	["graphics/freetype2"] =
+	{
+		wikidata = "Q1326617",
+		freshcode = "freetype"
+	},
+	["graphics/gdk-pixbuf2"] =
+	{
+		wikidata = "Q47272781"
+	},
+	["graphics/gimp"] =
+	{
+		wikidata = "Q8038",
+		freshcode = "gimp"
+	},
+	["graphics/geeqie"] =
+	{
+		wikidata = "Q9394384"
+	},
+	["graphics/jpeg"] =
+	{
+		wikidata = "Q131741"
+	},
+	["graphics/lcms2"] =
+	{
+		wikidata = "Q1502002"
+	},
+	["graphics/libexif"] =
+	{
+		wikidata = "Q28515920"
+	},
+	["graphics/libraw"] =
+	{
+		wikidata = "Q28517022"
+	},
+	["graphics/librsvg"] =
+	{
+		wikidata = "Q286024"
+	},
+	["graphics/libwebp"] =
+	{
+		wikidata = "Q63437758"
+	},
+	["graphics/openjpeg"] =
+	{
+		wikidata = "Q3353118"
+	},
+	["graphics/png"] =
+	{
+		wikidata = "Q838041"
+	},
+	["graphics/tiff"] =
+	{
+		wikidata = "Q1017110"
+	},
+	["mail/balsa"] =
+	{
+		wikidata = "Q401995"
+	},
+	["mail/mutt"] =
+	{
+		wikidata = "Q1457398"
+	},
+	["multimedia/libogg"] =
+	{
+		wikidata = "Q63437792"
+	},
+	["multimedia/vlc"] =
+	{
+		wikidata = "Q171477",
+		freshcode = "vlc"
+	},
+	["net/filezilla"] =
+	{
+		wikidata = "Q300662",
+		freshcode = "filezilla"
+	},
+	["net/haproxy"] =
+	{
+		wikidata = "Q5628948",
+		freshcode = "haproxy"
+	},
+	["net/remmina"] =
+	{
+		wikidata = "Q82148"
+	},
+	["net/rtorrent"] =
+	{
+		wikidata = "Q284842"
+	},
+	["net/syncthing"] =
+	{
+		wikidata = "Q17089344"
+	},
+	["net/transmission"] =
+	{
+		wikidata = "Q875055"
+	},
+	["security/gnutls"] =
+	{
+		wikidata = "Q1533305"
+	},
+	["security/libgcrypt"] =
+	{
+		wikidata = "Q3237868"
+	},
+	["security/putty"] =
+	{
+		wikidata = "Q841685",
+		freshcode = "putty"
+	},
+	["textproc/libxslt"] =
+	{
+		wikidata = "Q6542808"
+	},
+	["www/gitea"] =
+	{
+		wikidata = "Q28714270"
+	},
+	["www/lighttpd"] =
+	{
+		wikidata = "Q217765",
+		freshcode = "lighttpd"
+	},
+	["www/seamonkey"] =
+	{
+		wikidata = "Q156197",
+		freshcode = "seamonkey"
+	},
+	["www/thttpd"] =
+	{
+		wikidata = "Q3266371"
+	}
 }
